8da58959703dae30e1e3ca8732b2f847e691c6f6
Md Rafsan Janee <md.rafsanjanee.rjp@gmail.com>
2026-09-08 13:51:02 +0600

    Fixed NavBar Animation

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -28,7 +28,7 @@
 
       var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
-      if (scrollTop > 50) {
+      if (scrollTop > 20) {
         mainNav.classList.add("navbar-shrink");
       } else {
         mainNav.classList.remove("navbar-shrink");
